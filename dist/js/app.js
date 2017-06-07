f56188225759107ef3a34efc3bf882e11e58f3b7
rsdobbins <rsdobbins@gmail.com>
2017-06-06 18:20:15 -0600

    change reset btn style

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,7 @@
 var map;
 var marker;
-
-
-var point = function(name, lng, lat, wikiid, webUrl) {
+var googleError;
+var markerPoint = function(name, lng, lat, wikiid, webUrl) {
     var self = this;
     this.name = name;
     this.lng = lng;
@@ -13,14 +12,14 @@ var point = function(name, lng, lat, wikiid, webUrl) {
 };
 var viewModel = {
     resorts: [
-        new point("Winter Park Resort", 39.886927, -105.761325, "Winter_Park_Resort", "https://www.winterparkresort.com"),
-        new point("Snowshoe Mountain Resort", 38.410553, -79.993699, "Snowshoe_Mountain", "https://www.snowshoemtn.com"),
-        new point("Stratton Mountain Resort", 43.114917, -72.906929, "Stratton_Mountain_Resort", "https://www.stratton.com"),
-        new point("Blue Mountain Resort", 44.504930, -80.309992, "Blue_Mountain_(ski_resort)", "https://www.bluemountain.ca"),
-        new point("Tremblant Resort", 46.210167, -74.584993, "Mont_Tremblant_Resort", "https://www.tremblant.ca"),
-        new point("Steamboat Resort", 40.45669502741314, -106.80609941482544, "Steamboat_Ski_Resort", "https://www.steamboat.com")
+        new markerPoint("Winter Park Resort", 39.886927, -105.761325, "Winter_Park_Resort", "https://www.winterparkresort.com"),
+        new markerPoint("Snowshoe Mountain Resort", 38.410553, -79.993699, "Snowshoe_Mountain", "https://www.snowshoemtn.com"),
+        new markerPoint("Stratton Mountain Resort", 43.114917, -72.906929, "Stratton_Mountain_Resort", "https://www.stratton.com"),
+        new markerPoint("Blue Mountain Resort", 44.504930, -80.309992, "Blue_Mountain_(ski_resort)", "https://www.bluemountain.ca"),
+        new markerPoint("Tremblant Resort", 46.210167, -74.584993, "Mont_Tremblant_Resort", "https://www.tremblant.ca"),
+        new markerPoint("Steamboat Resort", 40.45669502741314, -106.80609941482544, "Steamboat_Ski_Resort", "https://www.steamboat.com")
     ],
-//observable used for running filter against resorts array
+    //observable used for running filter against resorts array
     filtered: ko.observable(''),
 };
 //console.log(viewModel);
@@ -43,7 +42,6 @@ viewModel.filteredResorts = ko.computed(function() {
         return matched;
     });
 }, viewModel);
-
 //load google map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -51,16 +49,14 @@ function initMap() {
         center: new google.maps.LatLng(38.941834, -89.532451),
         mapTypeId: google.maps.MapTypeId.ROAD,
     });
-//custom map markers
+    //custom map markers
     var iconMtn = {
-    url: '/img/icon_mtn.png',
-    size: new google.maps.Size(32, 22),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(0, 22)
-  };
-
+        url: '/img/icon_mtn.png',
+        size: new google.maps.Size(32, 22),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 22)
+    };
     var infowindow = new google.maps.InfoWindow({});
-    
     for (var i = 0; i < viewModel.resorts.length; i++) {
         var self = viewModel.resorts[i];
         viewModel.resorts[i].marker = new google.maps.Marker({
@@ -82,48 +78,45 @@ function initMap() {
             }, 2000);
             infowindow.setContent(marker.name);
             infowindow.open(map, marker);
-            var wikiUrl = 'https://en.wikipedia.org/wiki/' + marker.wikiID;
+            var wikiSourceUrl = 'https://en.wikipedia.org/wiki/' + marker.wikiID;
             //console.log(wikiSource);
             wikiUrl = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=1&exchars=150&titles=' + marker.wikiID + '&format=json&callback=wikiCallback';
-            var wikiError = setTimeout(function() {
-                $wikiExcerpt.text("Filed to load info");
-            }, 8000);
-           
-           //wiki JSONP call referenced fro classwork
+            //wiki JSONP call referenced fro classwork
             $.ajax({
                 url: wikiUrl,
                 dataType: 'jsonp',
-                success: function(data) {
-                    console.log(data);
-                    var excerpt = data.query.pages[Object.keys(data.query.pages)[0]].extract;
-                    infowindow.setContent('<div><a href=' + marker.webUrl + '><h4>' + marker.name + '</h4></a><p id="wikiExcerpt">' + excerpt + '</p><a href=' + wikiUrl + '>more</a><br><br>From ' + '<a href=' + wikiUrl + '>Wikipedia</a>, the free encyclopedia.' + '</div>');
-                    clearTimeout(wikiError);
-                }
-            })
+            }).done(function(data) {
+                console.log(data);
+                var excerpt = data.query.pages[Object.keys(data.query.pages)[0]].extract;
+                infowindow.setContent('<div><a href=' + marker.webUrl + '><h4>' + marker.name + '</h4></a><p id="wikiExcerpt">' + excerpt + '</p><a href=' + wikiSourceUrl + '>more</a><br><br>Source: ' + '<a href=' + wikiSourceUrl + '>Wikipedia</a>, the free encyclopedia.' + '</div>');
+            }).fail(function(jqXHR, textStatus) {
+                alert("Failed to load Wikpedia Content");
+            });
         };
         // Open infowindow upon click
         this.addListener = google.maps.event.addListener(self.marker, 'click', function() {
             openInfoWindow(this);
         });
     }
+    //reset list and map bounds
+    this.resetMarkers = function(reset) {
+        this.filtered('');
+        infowindow.close();
+        map.fitBounds(bounds);
+    };
     //Map bounds object
     var bounds = new google.maps.LatLngBounds();
     //Loop through an array of points, add them to bounds
-    for (var i = 0; i < viewModel.resorts.length; i++) {
-        var self = viewModel.resorts[i];
+    for (i = 0; i < viewModel.resorts.length; i++) {
+        self = viewModel.resorts[i];
         var geoCode = new google.maps.LatLng(self.lng, self.lat);
         bounds.extend(geoCode);
     }
     //Add new bounds object to map
     map.fitBounds(bounds);
     ko.applyBindings(viewModel);
-    //TODO reset teh list input on click. 
-    $("#reset_state").click(function() {
-        infowindow.close();
-        map.fitBounds(bounds);
-    });
-    // Show alert when Google Maps request fails 
-function googleError() {
-  alert("Map did not load");  
 }
+// Show alert when Google Maps request fails 
+function googleError() {
+    alert("Map did not load");
 }
